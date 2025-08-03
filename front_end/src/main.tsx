b46c4c6c1ae2d1f5fd7bf8db7fe8eb412dd45d4b
x w <nobodyiamjustnobody@outlook.com>
2025-08-03 16:54:32 +0100

    FE refine

--- front_end/src/main.tsx
+++ front_end/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// import App from "./App.tsx";
+import App from "./App.tsx";
 import {
-  // BrowserRouter,
+  BrowserRouter,
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
